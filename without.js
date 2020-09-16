baf3e9924cab4acd792b11c7b504200790945989
davidsmorton <davidsmorton@gmail.com>
2020-09-16 07:25:30 +0000

    removing a value from an array

---
+++ without.js
@@ -0,0 +1,36 @@
+
+let assertArraysEqual = function(array1, array2) {
+  //check if arrays are the same length
+  if (array1.length !== array2.length) {
+    return "These arrays dont' match, different lengths";
+  }
+  //check if array elements match in each array
+  for (let i = 0; i < array1.length; i++) {
+    //console.log(array1[i] !== array2[i]);
+    if (array1[i] !== array2[i]) {
+      return "These arrays dont' match, different values";
+    }
+    
+  }
+  return "These Arrays match";
+};
+
+let without = function(source, itemToRemove) {
+  let newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (source[i] !== itemToRemove[i]) {
+      newArray.push(source[i]);
+    //console.log(source);
+    }
+  }
+  return newArray;
+};
+
+
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
