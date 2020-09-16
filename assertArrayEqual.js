c2a746d413f7251a799e01e9da2d4ba187bf6318
davidsmorton <davidsmorton@gmail.com>
2020-09-16 06:03:02 +0000

    Function that compairs two arrays and prints message of compairson to console

---
+++ assertArrayEqual.js
@@ -0,0 +1,19 @@
+let assertArraysEqual = function(array1, array2) {
+  //check if arrays are the same length
+  if (array1.length !== array2.length) {
+    return "These arrays dont' match, different lengths";
+  }
+  //check if array elements match in each array
+  for (let i = 0; i < array1.length; i++) {
+    //console.log(array1[i] !== array2[i]);
+    if (array1[i] !== array2[i]) {
+      return "These arrays dont' match, different values";
+    }
+    
+  }
+  return "These Arrays match";
+};
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
